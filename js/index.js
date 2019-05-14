c4d225202e40679ee8d6899ccc361cf1c7eca237
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-05-14 16:09:26 +0100

    add mouser over and keydown event listners to web page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+const logoHeading = document.querySelector(".logo-heading");
+const contentSection = document.querySelector(".home .content-section");
+const inverseContent = document.querySelector(".home .inverse-content");
+const intro = document.querySelector('.intro h2');
+
+// fun bus color changes on mouse over 
+// and changes back to its initial color on mouse leave
+logoHeading.addEventListener("mouseover", () => {
+  logoHeading.setAttribute("style", "color:#FF9800");
+});
+
+logoHeading.addEventListener("mouseleave", () => {
+    logoHeading.setAttribute("style", "color:black");
+});
+
+// changes the position of the middle content. the image and text are switched
+window.addEventListener("keydown", () => {
+  contentSection.setAttribute("style", "flex-direction:row-reverse");
+  inverseContent.setAttribute("style", "flex-direction:row-reverse");
+});
